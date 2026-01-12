acb08657618005853c5802ac68c2692ecf0d8a56
kiana <hyy122013@outlook.com>
2026-01-13 00:29:17 +0800

    catppuccin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,33 +8,16 @@ function fish_user_key_bindings
     bind \cg 'set -l result (tv git-repos); and commandline -i -- $result; commandline -f repaint'
 end
 
-fish_config theme choose "tokyo-night-moon"
+fish_config theme choose "catppuccin-mocha"
 
 set -gx FZF_DEFAULT_OPTS "
   $FZF_DEFAULT_OPTS \
   --style full \
-  --highlight-line \
-  --info=inline-right \
-  --ansi \
-  --layout=reverse \
-  --border=none \
-  --color=bg+:#2d3f76 \
-  --color=bg:#1e2030 \
-  --color=border:#589ed7 \
-  --color=fg:#c8d3f5 \
-  --color=gutter:#1e2030 \
-  --color=header:#ff966c \
-  --color=hl+:#65bcff \
-  --color=hl:#65bcff \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#65bcff \
-  --color=query:#c8d3f5:regular \
-  --color=scrollbar:#589ed7 \
-  --color=separator:#ff966c \
-  --color=spinner:#ff007c \
-"
+  --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+  --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+  --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+  --color=selected-bg:#45475A \
+  --color=border:#6C7086,label:#CDD6F4"
 
 abbr -a l eza --icons -a --group-directories-first -1
 abbr -a ll eza --icons  -a --group-directories-first -1 --no-user --long

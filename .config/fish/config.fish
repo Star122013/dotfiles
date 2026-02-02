3c992e108834b0bd8a34dd523af112e1e9c82a0a
cyrene <hyy122013@outlook.com>
2026-02-02 17:10:19 +0800

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,17 +8,43 @@ function fish_user_key_bindings
     bind \cg 'set -l result (tv git-repos); and commandline -i -- $result; commandline -f repaint'
 end
 
-fish_config theme choose "catppuccin-mocha"
+function localrust
+    echo "🦀 进入 Rust 隔离环境 (输入 exit 退出)..."
+    # 启动一个新的 fish shell，带有修改后的 PATH
+    env PATH="$HOME/.cargo/bin:$PATH" fish
+    echo "👋 已退出 Rust 环境"
+end
 
-set -gx FZF_DEFAULT_OPTS "
+fish_config theme choose "eldritch"
+
+set -gx FZF_DEFAULT_OPTS " \
   $FZF_DEFAULT_OPTS \
-  --style full \
-  --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
-  --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
-  --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
-  --color=selected-bg:#45475A \
-  --color=border:#6C7086,label:#CDD6F4"
+  --height 40% --tmux bottom,40% --layout reverse \
+  --preview '~/.config/fzf/preview.sh {}' \
+  --style=full \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c
+"
 
+abbr -a sysyadm sudo yadm --yadm-dir /etc/yadm --yadm-data /etc/yadm/data
 abbr -a l eza --icons -a --group-directories-first -1
 abbr -a ll eza --icons  -a --group-directories-first -1 --no-user --long
 abbr -a tree eza --icons --tree --group-directories-first
